81893a72cd3913f575b97e3a0a1160979e1ab19f
Mourad NAKIB <mouradnakib@hotmail.com>
2021-07-10 14:50:50 +0400

    Added the connection script

---
+++ php-connectdb/index.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>Connection script to a database</h1>
+
+<?php
+$servername = $MYSQL_HOST;
+$username = "$MYSQL_USER;
+$password = $MYSQL_PASSWORD;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
+
+</body>
+</html> 
