ebe9b2fe8b631ca4271d58cd2b573f9663343468
Mourad NAKIB <39641630+mnakib@users.noreply.github.com>
2022-03-28 14:58:53 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -7,7 +7,9 @@
 <?php
 echo "Hello World! php version is " . PHP_VERSION . "\n";
   
-echo "This is a change in my PHP application";
+echo "This is a change in my PHP application"; <br />
+  
+echo "This application was created from the OCP Web UI";
 
 ?>
   
