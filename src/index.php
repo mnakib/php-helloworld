765db48652c89db0f2a38f03c1572d3c71a53de2
Mourad NAKIB <39641630+mnakib@users.noreply.github.com>
2022-03-28 14:28:05 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -6,10 +6,6 @@
 
 <?php
 echo "Hello World! php version is " . PHP_VERSION . "\n";
-echo "This is a change that has been done in my PHP code!\n";
-  
-
-echo "Let's see how S2I initiates the second build!\n";
 
 ?>
   
