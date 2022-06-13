8f1d8717e7456b3edda125f0265bc982adc71b77
Mourad NAKIB <39641630+mnakib@users.noreply.github.com>
2022-06-13 15:25:50 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
 <?php
 echo "Hello World! php version is " . PHP_VERSION . "\n\n\n";
   
-echo "Hello. This is an application deployed with S2I \n\n\n";
+echo "Hello. This is an application deployed with S2I \n\n\n. This change was proposed by Umair";
   
 echo "This is an update that is done to test upgrading a Source2Image application\n\n\n";
 
