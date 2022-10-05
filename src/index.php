effe3ff67de3f3cacf0447d0de754f044a0dda2a
Mourad NAKIB <39641630+mnakib@users.noreply.github.com>
2022-10-05 09:55:59 +0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,14 +5,13 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World! php version is " . PHP_VERSION . "\n\n\n";
+echo "<h1>Hello World! php version is " . PHP_VERSION . "</h1>\n\n\n";
   
-echo "Hello. This is an application deployed with S2I \n\n\n. This change was proposed by Umair";
+//echo "Hello. This is an application deployed with S2I \n\n\n.";
   
-echo "This is an update that is done to test upgrading a Source2Image application\n\n\n";
+//echo "This is an update that is done to test upgrading a Source2Image application\n\n\n";
 
   
-
 
 
 ?>
