a97f19c742b0b23760c2a05633101a774f3e7934
Mourad NAKIB <39641630+mnakib@users.noreply.github.com>
2025-08-03 00:40:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 <?php
 echo "Hello World!\n\n";
  
-echo "<h1>This is an update in our source code</h1>";
+# echo "<h1>This is an update in our source code</h1>";
  
-echo "<h1>This is an update numbered 2</h1>";
+# echo "<h1>This is an update numbered 2</h1>";
  
 ?>
 
 </body>
 </html> 
+
